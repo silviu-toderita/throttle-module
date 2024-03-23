ca960045fea38a262a11b8fe0de11bd628708b18
Andy Feng <andyfeng161@gmail.com>
2024-03-23 15:22:50 -0700

    Changes made to pin assignments

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,10 @@ uint32_t lastLedFlash = 0;
 
 bool ledFlash = false;
 
+uint16_t outputSamples[DATA_POINTS] = {0};
+int sampleIndex = 0;
+uint32_t rollingSum = 0;
+
 // This struct contains all the components of a CAN message. dataLength must be <= 8, 
 // and the first [dataLength] positions of data[] must contain valid data
 typedef uint8_t CanBuffer[8];
@@ -165,7 +169,16 @@ void loop() {
 
     // If the new output is different than the old output, update the voltage on the DAC
     if(newOutput != currentOutput) {
-        currentOutput = newOutput;
+
+        rollingSum -= outputSamples[sampleIndex];
+        rollingSum += newOutput;
+        outputSamples[sampleIndex] = newOutput;
+        sampleIndex = (sampleIndex + 1) % DATA_POINTS;
+
+        // Calculate the rolling average
+        uint16_t rollingAverage = rollingSum / DATA_POINTS;
+        
+        currentOutput = rollingAverage;
         dac.setVoltage(currentOutput, false); 
         DEBUG_SERIAL_LN("Voltage set to: " + String(currentOutput / DAC_VALUE_TO_V) + "v");
     }
@@ -177,6 +190,7 @@ void loop() {
         dac.setVoltage(currentOutput, false);
         DEBUG_SERIAL_LN("ERROR: NO DATA - Output set to 0v");
         ledFlash = false;
+        
         digitalWrite(PIN_LED, HIGH);
     }
 
@@ -199,4 +213,3 @@ void loop() {
     }
 
 }
-
--- src/settings.h
+++ src/settings.h
@@ -3,7 +3,7 @@
 #include "mcp2515_can.h"
 
 // Turn on to enable serial monitor
-#define DEBUG_SERIAL_EN         0
+#define DEBUG_SERIAL_EN         0 // Turn on
 #define SERIAL_MONITOR_SPEED    115200
 
 // After this time in ms of no throttle CAN messages, throttle set to 0
@@ -11,8 +11,8 @@
 // Heartbeat CAN message output interval
 #define HEARTBEAT_TIME          1000
 
-#define PIN_CAN_CS              2
-#define PIN_LED                 4
+#define PIN_CAN_CS              10
+#define PIN_LED                 5
 
 #define I2C_ADDR_DAC            0x60
 
@@ -21,10 +21,13 @@
 #define CAN_FRAME               0x0
 
 // Turn on to print received CAN messages to serial monitor
-#define CAN_DEBUG_RECEIVE       0
+#define CAN_DEBUG_RECEIVE       0 // Print to serial monitor every CAN message (turn on)
 
 // Turn on to scale exponentially
 #define SCALE_EXP               0
+
+// Rolling Average
+#define DATA_POINTS             20
 // Scaling factors
 #define DAC_VALUE_TO_V          819.2
 #define DAC_VALUE_TO_INPUT      16U
